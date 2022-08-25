7ee895309bc27fd9d2dc936d2f853fdcf61f6c5b
LAPTOP-SRMK0KVG\aam <gdfsdjj145@github.com>
2022-08-25 10:50:07 +0800

    init

--- packages/survey-ui/docs/.vitepress/theme/index.ts
+++ packages/survey-ui/docs/.vitepress/theme/index.ts
@@ -2,14 +2,12 @@ import DefaultTheme from 'vitepress/theme'
 import 'vitepress-theme-demoblock/theme/styles/index.css'
 import Demo from 'vitepress-theme-demoblock/components/Demo.vue'
 import DemoBlock from 'vitepress-theme-demoblock/components/DemoBlock.vue'
-import SRadio from '../../../packages/survey-ui/radio/Radio.vue'
-import SRadioInput from '../../../packages/survey-ui/radio/Radio-Input.vue'
+import SurveyUI from '../../../src/entry'
 
 export default {
   ...DefaultTheme,
   enhanceApp({app}) {
-    app.component('SRadio', SRadio),
-    app.component('SRadioInput', SRadioInput),
+    app.use(SurveyUI),
     app.component('Demo', Demo),
     app.component('DemoBlock', DemoBlock)
   }
